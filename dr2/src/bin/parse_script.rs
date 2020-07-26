d353ff6645e727e6d34039dca0ec309ce7b1ea17
Delta-Psi <delta.psi00@gmail.com>
2020-07-26 20:57:02 +0200

    full parsing

--- dr2/src/bin/parse_script.rs
+++ dr2/src/bin/parse_script.rs
@@ -2,5 +2,5 @@ fn main() {
     let filename = std::env::args().nth(1).unwrap();
     let file = std::fs::read(filename).unwrap();
     let script = std::str::from_utf8(&file).unwrap();
-    dr2::formats::lin::script_parser::parse(script).unwrap();
+    println!("{:?}", dr2::formats::lin::script_parser::parse(script).unwrap());
 }
--- dr2/src/formats/lin/script_parser.rs
+++ dr2/src/formats/lin/script_parser.rs
@@ -1,30 +1,56 @@
 use pest_derive::Parser;
 use crate::errors::*;
 
+#[derive(Debug)]
 pub enum Arg<'a> {
     Ident(&'a str),
     Int(i64),
     Text(&'a str),
 }
 
+#[derive(Debug)]
 pub struct Instr<'a> {
     pub operation: &'a str,
     pub args: Vec<Arg<'a>>,
 }
 
+#[derive(Debug)]
 pub struct Script<'a> {
     pub instrs: Vec<Instr<'a>>,
 }
 
-pub fn parse(input: &str) -> Result<()> {
+pub fn parse(input: &str) -> Result<Script> {
     use pest::Parser;
-    let result = ScriptParser::parse(Rule::script, input)?;
+    let mut result = ScriptParser::parse(Rule::script, input)?;
+    let instrs: Vec<_> = result.next().unwrap()
+        .into_inner()
+        .filter(|p| p.as_rule() == Rule::instr)
+        .map(|instr| {
+            let mut pairs = instr.into_inner();
+            let operation = pairs.next().unwrap().as_str();
+            
+            Instr {
+                operation,
+                args: pairs.filter(|p| p.as_rule() == Rule::arg)
+                    .map(|p| {
+                        let arg = p.into_inner().next().unwrap();
+                        match arg.as_rule() {
+                            Rule::ident => Arg::Ident(arg.as_str()),
+                            Rule::int_hex => Arg::Int(i64::from_str_radix(&arg.as_str()[2..], 16).unwrap()),
+                            Rule::int_dec => Arg::Int(arg.as_str().parse().unwrap()),
+                            Rule::text => Arg::Text(
+                                arg.into_inner().next().unwrap().as_str()
+                            ),
 
-    for instr in result {
-        println!("{:?}", instr);
-    }
+                            _ => unreachable!("invalid arg rule"),
+                        }
+                    }).collect(),
+            }
+        }).collect();
 
-    Ok(())
+    Ok(Script {
+        instrs,
+    })
 }
 
 #[derive(Parser)]
