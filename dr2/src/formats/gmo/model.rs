a4b38c47815f1139b5922e59cf5368f0f55cd23a
Delta-Psi <delta.psi00@gmail.com>
2020-08-02 15:10:29 +0200

    oops

--- dr2/src/formats/gmo/model.rs
+++ dr2/src/formats/gmo/model.rs
@@ -80,9 +80,9 @@ impl Chunk for Mesh {
                     for _ in 0..stripe_count {
                         for _ in 0..stripe_size/2 - 1 {
                             let a = LE::read_u16(&data[0..]);
-                            let c = LE::read_u16(&data[2..]);
-                            let b = LE::read_u16(&data[4..]);
-                            let d = LE::read_u16(&data[6..]);
+                            let b = LE::read_u16(&data[2..]);
+                            let d = LE::read_u16(&data[4..]);
+                            let c = LE::read_u16(&data[6..]);
 
                             faces.push((a, b, c, d));
                             data = &data[4..];
