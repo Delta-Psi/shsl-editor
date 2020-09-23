a4ba7c122a8cef9445371b0bd64022e4920788ae
Delta-Psi <delta.psi00@gmail.com>
2020-09-23 18:14:52 +0200

    branch reset

--- dr2/src/bin/parse_script.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let filename = std::env::args().nth(1).unwrap();
-    let file = std::fs::read(filename).unwrap();
-    let script = std::str::from_utf8(&file).unwrap();
-    let script = dr2::formats::lin::script_parser::parse_script(script).unwrap();
-    for instr in script.instrs {
-        println!("{:?}", instr);
-    }
-}
--- dr2/src/formats.rs
+++
@@ -1,6 +0,0 @@
-//! Procedures related to specific game data formats.
-
-pub mod lin;
-pub mod pak;
-pub mod tga;
-pub mod wad;
